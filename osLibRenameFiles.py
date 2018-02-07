6a808ee125caf0dcfb50bc8d711d850cbb253a2f
apard <apardo04@gmail.com>
2018-02-07 16:20:23 -0500

    adding read all files in deriectory

--- osLibRenameFiles.py
+++ osLibRenameFiles.py
@@ -8,11 +8,18 @@ import os
 os.path.isabs('.') #false
 os.path.isabs(os.path.abspath('.')) #true
 
-if os.path.exists('D:\Downloads\desktop_optimized'):
-    totalSize = 0
-    for filename in os.listdir('D:\Downloads\desktop_optimized'):
-        new_name = filename.replace("-min","")
-        old_file = os.path.join('D:\Downloads\desktop_optimized', filename)
-        new_file = os.path.join('D:\Downloads\desktop_optimized', new_name)
-        os.rename(old_file, new_file)
-    #print(totalSize)
+# FILE RENAME
+#if os.path.exists('D:\Downloads\desktop_optimized'):
+#    totalSize = 0
+#    for filename in os.listdir('D:\Downloads\desktop_optimized'):
+#        new_name = filename.replace("-min","")
+#        old_file = os.path.join('D:\Downloads\desktop_optimized', filename)
+#        new_file = os.path.join('D:\Downloads\desktop_optimized', new_name)
+#        os.rename(old_file, new_file)
+#    #print(totalSize)
+
+#---------------------------------------------
+# PRINT ALL FILES IN DIR
+if os.path.exists('C:\\Users\\apard\Downloads\\temp\\Rosas amazon Lola And Escarlata'):
+    for filename in os.listdir('C:\\Users\\apard\Downloads\\temp\\Rosas amazon Lola And Escarlata'):
+        print("https://cdn.globalrose.com/assets/img/amazon/" + filename)
